2aca16e871dcba567e1b5d3b8ed0462ae8ec40c9
Matheus de sousa dos Santos <matheusmtsm75@gmail.com>
2026-07-15 17:13:35 -0300

    refector: atualiza rotas, templetes e pacotes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+// import { Produto } from './components/produto/produto';
+import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ListaProdutos],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
