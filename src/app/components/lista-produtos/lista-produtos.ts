9b12e47e3ea3973ce51649bf611a04908e55c466
Matheus de sousa dos Santos <matheusmtsm75@gmail.com>
2026-07-15 17:12:27 -0300

    chore: Estrutura projeto Profissional

--- src/app/components/lista-produtos/lista-produtos.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-lista-produtos',
-  imports: [],
-  templateUrl: './lista-produtos.html',
-  styleUrl: './lista-produtos.css',
-})
-export class ListaProdutos {}
