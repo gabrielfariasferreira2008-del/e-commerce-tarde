d2219b045d33d4fe59e9011e0304a7ee7882acc9
gabriel farias ferreira <gabrielfariasferreira2008@gmail.com>
2026-08-20 16:55:06 -0300

    features atualizando o  checkout.html(@else if (!pedidoFinalizado()))

--- src/app/features/carrinho/carrinho/carrinho.ts
+++ src/app/features/carrinho/carrinho/carrinho.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject} from '@angular/core';
+import { ItemCarrinho } from '../../../core/models/itens.carrinho';
+
+import { MatButtonModule } from '@angular/material/button';
+import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
+
+import { RouterLink } from '@angular/router';
+import { CarrinhoFacade } from '../../../core/facades/carrinho.facade';
 
 @Component({
   selector: 'app-carrinho',
-  imports: [],
+  imports: [RouterLink, MatButtonModule],
   templateUrl: './carrinho.html',
   styleUrl: './carrinho.css',
 })
+
 export class Carrinho {
-  //Lógica do carrinho virá em aulas futuras
-  // por ora, apenas exibe a página de carrinho
+// A página do carrinho passa a consumir o estado global do carrinho.
+carrinhoFacade = inject(CarrinhoFacade);
+removerItem(indice: number) {
+// Remove um item específico da lista.
+this.carrinhoFacade.limparCarrinho();
+}
+
+  limparCarrinho() {{ 
+// Limpa todos os itens do carrinho.
+this.carrinhoFacade.limparCarrinho();
+}};
 }
