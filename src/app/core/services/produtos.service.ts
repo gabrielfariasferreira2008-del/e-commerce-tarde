49f15f941454f82238bab7b4810aabcb44b29f44
gabriel farias ferreira <gabrielfariasferreira2008@gmail.com>
2026-08-20 16:30:55 -0300

    features modifixando o produtoService.ts

--- src/app/core/services/produtos.service.ts
+++ src/app/core/services/produtos.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
 import { Inject } from "@angular/core";
-
 type ProdutoApi = {
     title: string;
     price: number;
